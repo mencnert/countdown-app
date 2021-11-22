c858fb5126432055bc4518597f27f361bea5d37a
tomas.mencner <mencnert@gmail.com>
2021-11-22 15:03:51 +0100

    timestamp picker

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import { formatDurationV2 } from "./utils"
 function App() {
   const [duration, setDuration] = React.useState(0)
   const [time, setTime] = React.useState("")
+  const [pickedTime, setPickedTime] = React.useState("")
+  const pickDateTime = (e) => {
+    setPickedTime(e.target.value)
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
+    window.location.search = `?T=${pickedTime}`
+  }
   React.useEffect(() => {
     var url_string = window.location.href
     var url = new URL(url_string);
@@ -15,21 +23,35 @@ function App() {
     const intervalId = setInterval(() => {
       const launch = new Date(time)
       const now = Date.now()
+      const diff = Math.round((now - launch) / 1000);
       console.clear()
       console.log("T:    ", launch.toISOString())
       console.log("Now:  ", new Date(now).toISOString())
-      const diff = Math.round((now - launch) / 1000);
+      console.log("Dur:  ", formatDurationV2(diff))
       console.log("Diff: ", diff)
       setDuration(diff)
     }, 1000)
 
     return () => clearInterval(intervalId);
   }, [time])
-  return (
-    <div className="App">
-      T {formatDurationV2(duration)}
-    </div>
-  );
+
+  if (time) {
+    return (
+      <div className="App">
+        T {formatDurationV2(duration)}
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <form onSubmit={onSubmit}>
+          <input type="datetime-local" value={pickedTime} onChange={pickDateTime} required />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
+
 
 export default App;
